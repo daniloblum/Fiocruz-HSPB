a30044fa68a862f524dba3362b86ee1f03ff1fc5
Danilo Blum <dnl.blm@gmail.com>
2021-06-10 18:10:07 -0300

    animacoes em cards e timeline

--- js/custom-anime.js
+++ js/custom-anime.js
@@ -1,13 +1,85 @@
-// Boxes
+// Instant Animation
 
-var title = document.querySelector(".header__title")
+const headerTitleLabel = document.querySelector('.header__title h3')
+const headerTitle = document.querySelector('.header__title h1')
 
-var headerTitle = function (){
-
-    anime({
-        targets: title,
-        translateX: 250,
-        opacity: 0
-        
-      })
+function animate (element, type,  delay) {
+  element.classList.add('animate__animated','animate__' + type)
+  element.style.animationDelay = delay
 }
+
+animate(headerTitleLabel, "fadeIn", '0.2s')
+animate(headerTitle, "fadeIn", '0.4s')
+
+
+// Intersection Observer Animation - CARDS
+
+const cards = document.querySelectorAll('.card')
+const timelineCards = document.querySelectorAll('.cd-timeline__content')
+
+const options = {
+  root: null, //it is the viewport
+  rootMargin: "-150px",
+  threshold: 0,
+};
+
+const observerCards = new IntersectionObserver (function (entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // console.log(entry.target)
+      entry.target.classList.add("card--in")
+      entry.target.classList.remove("card--out")
+    } else {
+      // console.log(entry.target)
+      entry.target.classList.remove("card--in")
+      entry.target.classList.add("card--out")
+    }
+  })
+}, options);
+
+cards.forEach(card => {
+  observerCards.observe(card);
+})
+
+// Intersection Observer Animation - CARDS
+
+const optionsTimeline = {
+  root: null, //it is the viewport
+  rootMargin: "0px",
+  threshold: 0,
+};
+
+const observerTimeline = new IntersectionObserver (function (entries, optionsTimeline) {
+  entries.forEach(entry => {
+
+      if (entry.isIntersecting) {
+        //console.log(entry.target)
+        entry.target.classList.add("animate__animated")
+
+        if (entry.target.classList.contains("bounce-left")) {
+          entry.target.classList.remove("animate__fadeOutLeft")
+          entry.target.classList.add("animate__fadeInLeft")  
+        } else {
+          entry.target.classList.remove("animate__fadeOutRight")
+          entry.target.classList.add("animate__fadeInRight")  
+        }
+        
+      } else {
+        entry.target.classList.add("animate__animated")
+        
+        if (entry.target.classList.contains("bounce-left")) {
+          entry.target.classList.remove("animate__fadeInLeft")
+          entry.target.classList.add("animate__fadeOutLeft")  
+        }
+        else {
+          entry.target.classList.remove("animate__fadeInRight")
+          entry.target.classList.add("animate__fadeOutRight")  
+        }
+      }
+    
+  })
+}, optionsTimeline);
+
+timelineCards.forEach(card => {
+  observerTimeline.observe(card);
+})
